8c9ead5bf1dfb10677040fc6f8be935fd82eb1c0
lemokh <byndasma@gmail.com>
2019-08-21 10:30:39 -1000

    adding sign in forms and setting up firebase auth

--- src/App.js
+++ src/App.js
@@ -4,16 +4,36 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component.jsx";
-import Header from "./components/header/header.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sing-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
+import { auth } from "./firebase/firebase-utils";
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+}
+
+componentDidMount() {
+  auth.onAuthStateChanged(user => {
+    this.setState = ({ currentUser: user });
+    console.log(user);
+  });
+}
+
+render() {
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyAYQYz_Xp_J7wjKM_bL4xsXBrqvomgYANU",
+  authDomain: "local-joe-db.firebaseapp.com",
+  databaseURL: "https://local-joe-db.firebaseio.com",
+  projectId: "local-joe-db",
+  storageBucket: "",
+  messagingSenderId: "208802152552",
+  appId: "1:208802152552:web:185b00ba696c8886"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestorm();
+
+const provider = new firebase.auto.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
