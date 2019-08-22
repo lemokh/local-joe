98dfb8843cabdda55cb9679790188be4cd61170b
lemokh <byndasma@gmail.com>
2019-08-21 19:28:00 -1000

    debugged all typos

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,9 +15,9 @@ const config = {
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-export const firestore = firebase.firestorm();
+export const firestore = firebase.firestore();
 
-const provider = new firebase.auto.GoogleAuthProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
