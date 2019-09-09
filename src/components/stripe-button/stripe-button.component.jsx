fcb3ebfa5386e5968a3eafb8339224441c4950ca
lemokh <byndasma@gmail.com>
2019-09-08 18:36:46 -1000

    integrated app with dummy Stripe api key then updated favicon.cio and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+
+import "./stripe-button.styles.scss";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_jW3zmhFPY3tGBhUUk99UwgRv008Lwuldbx";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Local Joe"
+      billingAddress
+      shippingAddress
+      image="https://svgshare/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
