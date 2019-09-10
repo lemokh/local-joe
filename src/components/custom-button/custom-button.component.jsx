c0a9d65e9f6b2c57e65fd4a5da82270eb6a2f578
lemokh <byndasma@gmail.com>
2019-09-10 08:01:08 -1000

    implemented some examples of styled-components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 
-import "./custom-button.styles.scss";
+import { customButtonContainer } from "./custom-button.styles";
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  <button
-    className={`${inverted ? "inverted" : ""} custom-button`}
-    {...otherProps}
-  >
-    {children}
-  </button>
+const CustomButton = ({ children, ...props }) => (
+  <customButtonContainer {...props}>{children}</customButtonContainer>
 );
 
 export default CustomButton;
