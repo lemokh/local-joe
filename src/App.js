f44463dea08bd21b0d5407bc89a2e798682ae6e2
lemokh <byndasma@gmail.com>
2020-11-18 11:09:51 +0400

    rebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -80,3 +81,30 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(App);
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> Initial commit from Create React App
